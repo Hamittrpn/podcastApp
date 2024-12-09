966d7a5cd929dea5d0b0a7cdc4b057a73c2201f2
hamittrpn <hamittirpan@gmail.com>
2024-12-10 02:01:28 +0300

    :sparkles: feat: add first version of project

---
+++ podcastApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  podcastApp
+//
+//  Created by Hamit Tırpan on 10.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+    }
+}
---
+++ podcastAppTests/podcastAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  podcastAppTests.swift
+//  podcastAppTests
+//
+//  Created by Hamit Tırpan on 10.12.2024.
+//
+
+import Testing
+@testable import podcastApp
+
+struct podcastAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
